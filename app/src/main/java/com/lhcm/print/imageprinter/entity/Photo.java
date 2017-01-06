9871ce899c2f75f3805e0324271157de3c156363
chenweiqi <hello_v7@163.com>
2017-01-06 15:21:36 +0800

    AI-2.2.3 <chenweiqi@chenweiqideMacBook-Pro.local Overwrite remote https://github.com/cwq1214/ImagePrinter.git to local

--- app/src/main/java/com/lhcm/print/imageprinter/entity/Photo.java
+++
@@ -1,9 +0,0 @@
-package com.lhcm.print.imageprinter.entity;
-
-/**
- * Created by chenweiqi on 2017/1/4.
- */
-
-public class Photo {
-
-}
--- app/src/main/java/com/lhcm/print/imageprinter/ui/activity/EditAddressActivity.java
+++
@@ -1,8 +0,0 @@
-package com.lhcm.print.imageprinter.ui.activity;
-
-/**
- * Created by chenweiqi on 2017/1/5.
- */
-
-public class EditAddressActivity {
-}
